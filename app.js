85d8a1c26714cae8a50f90dfb0b3c722dd2c48f1
Cristal Tay <bluecolorpencils@gmail.com>
2016-08-30 15:26:01 -0700

    added port listener

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(function(err, req, res, next) {
 
 
 
-app.listen(3000);
-console.log('Server started on port 3000');
+app.listen(process.env.PORT || 3000);
+// console.log('Server started on port 3000');
 
 module.exports = app;
